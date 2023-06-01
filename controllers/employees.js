67601e62432b55e7b583f206d257ffc3378fed96
HamJohni <joni.pacan.wf@gmail.com>
2023-06-01 16:57:16 +0600

    get__employee

--- controllers/employees.js
+++ controllers/employees.js
@@ -103,7 +103,7 @@ const employee = async (req,res) => {
             }
         })
 
-        res.status(200).json()
+        res.status(200).json(employee)
 
     }catch (err){
         res.status(400).json({message: "Не удалось получить сотрудника..."})
